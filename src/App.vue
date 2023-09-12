c24fb98cddbafe4852045bd3103057dfa2998030
Legui <leguigol@hotmail.com>
2023-09-12 12:14:40 -0300

    comit desde farmacia ultimo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
     <a-layout class="custom-layout">
-      <a-layout-header :style="{ backgroundColor: 'white', marginTop: '200px', marginLeft: '20px', marginRight: '20px'}" v-if="!userStore.loadingSession">
+      <a-layout-header :style="{ padding: 0, marginTop: isSmallScreen ? '130px' : resolucion, marginLeft: '30px', marginRight: '30px', textAlign: 'center' }" v-if="!userStore.loadingSession">
         <a-menu 
           mode="horizontal"
           :style="{ lineHeight: '64px'}"
           v-model:selectedKeys="selectedKeys"
-
         >
             <a-menu-item v-if="userStore.userData" key="home">
               <router-link to="/">Home</router-link>        
@@ -24,11 +23,12 @@
           :style="{
             background: '#fff',
             padding: '24px',
-            minHeight: '200px',
           }"        
         >
           <div v-if="userStore.loadingSession">loading user....</div>
           <router-view></router-view>
+          <!-- <div class="relleno">
+          </div> -->
         </div>
       </a-layout-content>
   </a-layout>
@@ -39,19 +39,21 @@ body {
   margin: 0;
   padding: 0;
 }
+.relleno {
+  height: 1px;
+}
 .custom-layout {
 
-  background-image: url('./assets/credencial_blanco.JPEG'); /* Cambia la ruta a la imagen */
-  background-size: 100% auto;
-  background-position: center center;
-  min-height: 100vh;
-  background-repeat: no-repeat;
-  position: absolute;
+  background-image: url('./assets/credencial_blanco_768.jpg'); 
+  background-repeat: no-repeat; 
+  width: 100%;
+  background-size: cover;
+  /* background-position: center center;
   /* Otros estilos si es necesario */
 }
 </style>
 <script setup>
-import { ref, watch } from 'vue';
+import { onMounted, ref, computed, watch, onBeforeUnmount } from 'vue';
 import {useUserStore} from './stores/user';
 import { useRoute } from 'vue-router';
 
@@ -59,6 +61,36 @@ const userStore=useUserStore();
 const route=useRoute();
 
 const selectedKeys=ref([''] );
+const isSmallScreen = ref(false);
+const resolucion=ref('');
+
+if (isSmallScreen.value) {
+  headerStyle.value.marginTop = '100px';
+}
+const headerStyle = ref({
+  padding: '0',
+  marginTop: '200px'
+  });
+
+const checkScreenSize = () => {
+  isSmallScreen.value = window.innerWidth < 650;
+  let valu=parseInt(window.innerWidth)-400;
+  resolucion.value=valu+"px";
+  
+  console.log('Ancho de pantalla:',resolucion.value);
+};
+
+// Verificar tamaño de pantalla al montar el componente
+onMounted(() => {
+  checkScreenSize();
+  window.addEventListener('resize', checkScreenSize);
+});
+
+// Remover el evento al desmontar el componente
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', checkScreenSize);
+});
+
 
 watch( 
   ()=> route.name, 
